564012031e0b4f5edc9b2944e23bc2ba5b88258f
dejo_17 <dean.juricic@gmail.com>
2019-06-21 09:57:43 +0200

    initial commit

---
+++ src/main/java/hr/scorpiusmobile/springmvcrestmongoreactive/SpringrestreactiveApplication.java
@@ -0,0 +1,13 @@
+package hr.scorpiusmobile.springmvcrestmongoreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestreactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestreactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/scorpiusmobile/springmvcrestmongoreactive/SpringrestreactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.scorpiusmobile.springmvcrestmongoreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestreactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
